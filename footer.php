407f70647c846d2ffbe613f7fd8ffaa1347f20a0
Teodoro Salgado Mac <tsalgado96@gmail.com>
2017-06-02 01:16:46 -0400

    No longer using CDN's to link bootstrap or jquery.

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
     <?php wp_footer(); ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/jquery-3.2.1.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
   </body>
 </html>
